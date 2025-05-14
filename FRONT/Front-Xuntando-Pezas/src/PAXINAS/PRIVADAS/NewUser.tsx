9c509a1331c262d6d357599ed247a11ae536a2e8
AdriConxo <adri82cx@gmail.com>
2025-05-14 20:54:00 +0200

    Datos enviados y recibidos

--- FRONT/Front-Xuntando-Pezas/src/PAXINAS/PRIVADAS/NewUser.tsx
+++ FRONT/Front-Xuntando-Pezas/src/PAXINAS/PRIVADAS/NewUser.tsx
@@ -1,4 +1,4 @@
-// src/paginas/NewUser.tsx
+
 import React, { useState } from "react";
 import "../../Componentes/PaxinaNewUser/index-newuser.css";
 import Formulario from "../../Componentes/PaxinaNewUser/Formulario";
@@ -6,7 +6,8 @@ import ImaxenEngranaxe from "../../Componentes/PaxinaNewUser/ImaxenEngranaxe";
 import Avatar from "../../Componentes/PaxinaNewUser/Avatar";
 import BotonEnviar from "../../Componentes/PaxinaNewUser/BotonEnviar";
 import { Imaxes } from "../../assets/imaxes_newuser";
-import MenuLateral from "../../Componentes/MenuLateral";
+import { DatosNewUser } from "../../TIPOS/INTERFACES.NewUser";
+
 
 /*const NewUser = ()=>{
     return <div className="estilo-paxinas-app">
@@ -16,7 +17,7 @@ import MenuLateral from "../../Componentes/MenuLateral";
 
 export default function NewUser() {
   const [imagenPerfil, setImagenPerfil] = useState<string>(Imaxes.avatar);
-  const [datosFormulario, setDatosFormulario] = useState<DatosFormulario>({});
+  const [datosFormulario, setDatosFormulario] = useState<DatosNewUser>({});
 
   const handleSubmit = async () => {
     const datos = { ...datosFormulario, imagen: imagenPerfil };
